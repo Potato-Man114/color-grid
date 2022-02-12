e8f0d0edb3c8482db8fd9894e64b110d2678fab5
Matthew Hill <potatoman.mh@gmail.com>
2022-02-11 22:59:08 -0700

    Initialize repo with minimum functionality

---
+++ README.md
@@ -0,0 +1,12 @@
+# Color Grid
+## Concept
+
+It's really quite simple; it's just a grid of colors!
+
+## Purpose
+
+This was created in an attempt to familiarize myself with the `<canvas>` HTML element and it's use in JS.
+
+## Result
+
+A rather laggy grid of colors based on the user's window size.
---
+++ script.js
@@ -0,0 +1,60 @@
+container = document.querySelector("#container");
+
+class Color {
+    constructor(r, g, b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
+
+function start() {
+    rows = Math.floor((window.innerHeight - 100) / 50);
+    cols = Math.floor((window.innerWidth - 25) / 50);
+    console.log(window.innerWidth);
+    console.log(cols);
+    colors = getColors(5);
+    generate(rows, cols, colors);
+}
+
+function generate(rows, cols, colors) {
+    for (let row = 0; row < rows; row++) {
+        currentRow = document.createElement("div");
+        currentRow.setAttribute("class", "row");
+        for (let col = 0; col < cols; col++) {
+            currentCellSpan = document.createElement("span");
+            currentCellSpan.setAttribute("class", "cell");
+
+            currentCellCanvas = document.createElement("canvas");
+            currentCellCanvas.textContent = ":(";
+            currentCellCanvas.setAttribute("class", "cell__canvas");
+            currentCellCanvas.setAttribute("width", "50");
+            currentCellCanvas.setAttribute("height", "50");
+
+            currentCellSpan.appendChild(currentCellCanvas);
+            currentRow.appendChild(currentCellSpan);
+
+            context = currentCellCanvas.getContext("2d");
+            let color = colors[Math.floor((Math.random() * 1000)) % colors.length];
+            context.fillStyle=`#${color.r}${color.g}${color.b}`;
+            context.fillRect(0,0,50,50);
+
+            currentCellSpan            
+        }
+        container.appendChild(currentRow);
+    }
+}
+
+function getColors(num) {
+    let colors = [];
+    for (let i = 0; i < num; i++) {
+        let r = Math.floor((Math.random() * 1000)) % 256;
+        let g = Math.floor((Math.random() * 1000)) % 256;
+        let b = Math.floor((Math.random() * 1000)) % 256;
+        colors.push(new Color(r.toString(16), g.toString(16), b.toString(16)));
+    }
+    console.log("colors: " + colors);
+    return colors;
+}
+
+start();
